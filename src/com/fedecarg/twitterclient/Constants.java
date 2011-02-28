d6a45cf087c56a7854202d132b7362c1ab4d0b47
fede <fede@80e17c93-eb1c-44a2-a00e-b391dc33b984>
2011-02-28 12:55:00 +0000

    Added Constants.java
    git-svn-id: http://svn.fedecarg.com/repo/Android/TwitterClient@189 80e17c93-eb1c-44a2-a00e-b391dc33b984

---
+++ src/com/fedecarg/twitterclient/Constants.java
@@ -0,0 +1,16 @@
+package com.fedecarg.twitterclient;
+
+public class Constants {
+
+	public static final String CONSUMER_KEY = "";
+	public static final String CONSUMER_SECRET = "";
+
+	public static final String REQUEST_URL = "http://api.twitter.com/oauth/request_token";
+	public static final String ACCESS_URL = "http://api.twitter.com/oauth/access_token";
+	public static final String AUTHORIZE_URL = "http://api.twitter.com/oauth/authorize";
+
+	public static final String OAUTH_CALLBACK_SCHEME = "x-oauthflow-twitter";
+	public static final String OAUTH_CALLBACK_HOST = "callback";
+	public static final String OAUTH_CALLBACK_URL = OAUTH_CALLBACK_SCHEME + "://" + OAUTH_CALLBACK_HOST;
+
+}
